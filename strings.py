8b78e9a538658af77735aa4d8af24fa672db11c4
phlshenrique <tecpedrohl@gmail.com>
2022-02-03 00:06:28 -0300

    Strings

---
+++ strings.py
@@ -0,0 +1,17 @@
+name = "pedro henrique"
+
+#first letter 
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+
+first_name = "pedro"
+last_name = "lopes"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+#first letter of every word
+print(full_name.title())
+
+some_number = 0.1 + 0.2
+#Formatting a float number
+print("{.2f}".format(some_number))
