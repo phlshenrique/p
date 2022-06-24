7469cd2cd8a1dbda767ffcf6cf7c0c45f8878217
phlshenrique <tecpedrohl@gmail.com>
2022-06-24 16:25:31 -0300

    function with keywords arguments

---
+++ functions.py
@@ -0,0 +1,5 @@
+def describe_pet(animal_type, animal_name):
+    print(f"\nI have a {animal_type}")
+    print(f"My {animal_type}'s name is: {animal_name}.")
+
+describe_pet(animal_type="Dog", animal_name="Mimosa")
